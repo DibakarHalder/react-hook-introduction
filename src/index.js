b453d35c9d928e5700a2ef189ab527978c76ea49
Dibakar Halder <dibakar.haider@partners.rivigo.com>
2019-04-05 12:04:59 +0530

    added App with basic fetch api call to display news

--- src/index.js
+++ src/index.js
@@ -1,14 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
+import App from './App';
 
 const rootElement = document.getElementById('app');
-function App() {
-    return (
-        <>
-            <h1>News</h1>
-        </>
-    );
-}
 
 render(<App />, rootElement);
 
